953a50a161bfb8181dc9bd3fae0d31dd5807dcaa
Unknown <christopher.nyberg@gmail.com>
2017-03-05 00:44:55 -0800

    Added a simple command line interface

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello")
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "listens on the specified IP address")
+
+	flag.Parse()
+
+	if isHost {
+		// go run main.go -listen <ip>
+		os.Args
+		fmt.Println("is host")
+	} else {
+		// go run main.go <ip>
+		fmt.Println("is guest")
+	}
 }
